07495b431d4600a48e1c50dc6af96c9a75fb1a86
fernando <fernandool2@hotmail.com>
2022-07-14 23:44:36 -0300

    Parte 5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 const cards =  document.querySelectorAll('.card');
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let lockBoard = false;
 
 function flipCard() {
+    if(lockBoard) return;
+    
     this.classList.add('flip');
     if (!hasFlippedCard ) {
         hasFlippedCard = true;
@@ -23,6 +26,18 @@ function chekForMatch(){
 
 
      unflipCard();    
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click',flipCard);
+    secondCard.removeEventListener('click',flipCard);
+}
+
+function unflipCard() {
+    setTimeout(() =>{
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500);
 }
 
 cards.forEach((card) => {
